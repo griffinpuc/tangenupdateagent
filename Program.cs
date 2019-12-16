a2421a71d3b2688bcb8bf9f7f2f650d069a4173b
Griffin Puc <griffinpuc@outlook.com>
2019-12-16 11:02:25 -0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tdp_update_agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
